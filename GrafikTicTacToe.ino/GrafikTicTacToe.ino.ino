9956f7cf83da8beabb4e39633e04ee20ea03067c
Waios <waios.floros@hotmail.de>
2018-03-01 15:20:22 +0100

    mit arduino code

---
+++ GrafikTicTacToe.ino/GrafikTicTacToe.ino.ino
@@ -0,0 +1,186 @@
+
+#include <Elegoo_GFX.h>    // Core graphics library
+#include <Elegoo_TFTLCD.h> // Hardware-specific library
+#include <TouchScreen.h>
+
+#if defined(__SAM3X8E__)
+    #undef __FlashStringHelper::F(string_literal)
+    #define F(string_literal) string_literal
+#endif
+
+
+/*
+#define YP 9  // must be an analog pin, use "An" notation!
+#define XM 8  // must be an analog pin, use "An" notation!
+#define YM A2   // can be a digital pin
+#define XP A3   // can be a digital pin
+*/
+
+#define YP A3  // must be an analog pin, use "An" notation!
+#define XM A2  // must be an analog pin, use "An" notation!
+#define YM 9   // can be a digital pin
+#define XP 8   // can be a digital pin
+/*
+#define TS_MINX 50
+#define TS_MAXX 920
+
+#define TS_MINY 100
+#define TS_MAXY 940
+*/
+//Touch For New ILI9341 TP
+#define TS_MINX 120
+#define TS_MAXX 900
+
+#define TS_MINY 70
+#define TS_MAXY 920
+
+// For better pressure precision, we need to know the resistance
+// between X+ and X- Use any multimeter to read it
+// For the one we're using, its 300 ohms across the X plate
+TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
+
+#define LCD_CS A3
+#define LCD_CD A2
+#define LCD_WR A1
+#define LCD_RD A0
+// optional
+#define LCD_RESET A4
+
+// Assign human-readable names to some common 16-bit color values:
+#define	BLACK   0x0000
+#define	BLUE    0x001F
+#define	RED     0xF800
+#define	GREEN   0x07E0
+#define CYAN    0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW  0xFFE0
+#define WHITE   0xFFFF
+
+
+Elegoo_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
+
+#define BOXSIZE 40
+#define PENRADIUS 3
+int oldcolor, currentcolor;
+
+void setup(void) {
+  Serial.begin(9600);
+  Serial.println(F("Paint!"));
+  
+  tft.reset();
+  
+  uint16_t identifier = tft.readID();
+  if(identifier == 0x9325) {
+    Serial.println(F("Found ILI9325 LCD driver"));
+  } else if(identifier == 0x9328) {
+    Serial.println(F("Found ILI9328 LCD driver"));
+  } else if(identifier == 0x4535) {
+    Serial.println(F("Found LGDP4535 LCD driver"));
+  }else if(identifier == 0x7575) {
+    Serial.println(F("Found HX8347G LCD driver"));
+  } else if(identifier == 0x9341) {
+    Serial.println(F("Found ILI9341 LCD driver"));
+  } else if(identifier == 0x8357) {
+    Serial.println(F("Found HX8357D LCD driver"));
+  } else if(identifier==0x0101)
+  {     
+      identifier=0x9341;
+       Serial.println(F("Found 0x9341 LCD driver"));
+  }else {
+    Serial.print(F("Unknown LCD driver chip: "));
+   
+    identifier=0x9341;
+   
+  }
+
+  tft.begin(identifier);
+  tft.setRotation(2);
+
+  tft.fillScreen(BLACK);
+
+  tft.drawLine(20, 120, 220, 120, YELLOW);
+  tft.drawLine(20, 200, 220, 200, YELLOW);
+  tft.drawLine(82, 40, 82, 280, WHITE);
+  tft.drawLine(162, 40, 162, 280, WHITE);
+  
+  pinMode(13, OUTPUT);
+}
+
+#define MINPRESSURE 10
+#define MAXPRESSURE 1000
+
+void loop()
+{
+  digitalWrite(13, HIGH);
+  TSPoint p = ts.getPoint();
+  digitalWrite(13, LOW);
+
+  // if sharing pins, you'll need to fix the directions of the touchscreen pins
+  //pinMode(XP, OUTPUT);
+  pinMode(XM, OUTPUT);
+  pinMode(YP, OUTPUT);
+  //pinMode(YM, OUTPUT);
+
+  // we have some minimum pressure we consider 'valid'
+  // pressure of 0 means no pressing!
+
+  if (p.z > MINPRESSURE && p.z < MAXPRESSURE) {
+    /*
+    Serial.print("X = "); Serial.print(p.x);
+    Serial.print("\tY = "); Serial.print(p.y);
+    Serial.print("\tPressure = "); Serial.println(p.z);
+    */
+    
+    if (p.y < (TS_MINY-5)) {
+      Serial.println("erase");
+      // press the bottom of the screen to erase 
+      tft.fillRect(0, BOXSIZE, tft.width(), tft.height()-BOXSIZE, BLACK);
+    }
+    // scale from 0->1023 to tft.width
+    p.x = map(p.x, TS_MINX, TS_MAXX, tft.width(), 0);
+    //p.x = tft.width()-map(p.x, TS_MINX, TS_MAXX, tft.width(), 0);
+    p.y = (tft.height()-map(p.y, TS_MINY, TS_MAXY, tft.height(), 0));
+     //p.y = map(p.y, TS_MINY, TS_MAXY, tft.height(), 0);
+    /*
+    Serial.print("("); Serial.print(p.x);
+    Serial.print(", "); Serial.print(p.y);
+    Serial.println(")");
+    */
+    if (p.y < BOXSIZE) {
+       oldcolor = currentcolor;
+
+       if (p.x < BOXSIZE) { 
+         currentcolor = RED; 
+         tft.drawRect(0, 0, BOXSIZE, BOXSIZE, WHITE);
+       } else if (p.x < BOXSIZE*2) {
+         currentcolor = YELLOW;
+         tft.drawRect(BOXSIZE, 0, BOXSIZE, BOXSIZE, WHITE);
+       } else if (p.x < BOXSIZE*3) {
+         currentcolor = GREEN;
+         tft.drawRect(BOXSIZE*2, 0, BOXSIZE, BOXSIZE, WHITE);
+       } else if (p.x < BOXSIZE*4) {
+         currentcolor = CYAN;
+         tft.drawRect(BOXSIZE*3, 0, BOXSIZE, BOXSIZE, WHITE);
+       } else if (p.x < BOXSIZE*5) {
+         currentcolor = BLUE;
+         tft.drawRect(BOXSIZE*4, 0, BOXSIZE, BOXSIZE, WHITE);
+       } else if (p.x < BOXSIZE*6) {
+         currentcolor = MAGENTA;
+         tft.drawRect(BOXSIZE*5, 0, BOXSIZE, BOXSIZE, WHITE);
+       }
+
+       if (oldcolor != currentcolor) {
+          if (oldcolor == RED) tft.fillRect(0, 0, BOXSIZE, BOXSIZE, RED);
+          if (oldcolor == YELLOW) tft.fillRect(BOXSIZE, 0, BOXSIZE, BOXSIZE, YELLOW);
+          if (oldcolor == GREEN) tft.fillRect(BOXSIZE*2, 0, BOXSIZE, BOXSIZE, GREEN);
+          if (oldcolor == CYAN) tft.fillRect(BOXSIZE*3, 0, BOXSIZE, BOXSIZE, CYAN);
+          if (oldcolor == BLUE) tft.fillRect(BOXSIZE*4, 0, BOXSIZE, BOXSIZE, BLUE);
+          if (oldcolor == MAGENTA) tft.fillRect(BOXSIZE*5, 0, BOXSIZE, BOXSIZE, MAGENTA);
+       }
+    }
+    if (((p.y-PENRADIUS) > BOXSIZE) && ((p.y+PENRADIUS) < tft.height())) {
+      tft.fillCircle(p.x, p.y, PENRADIUS, currentcolor);
+    }
+  }
+}
+
